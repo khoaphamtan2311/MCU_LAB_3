83388f319ed54e554337407add423f53bb9a1875
khoaphamtan2311 <khoaphamtan2311@gmail.com>
2024-10-29 05:08:31 +0700

    first commit Pham Tan Khoa 2252359

---
+++ Src/global.c
@@ -0,0 +1,23 @@
+/*
+ * global.c
+ *
+ *  Created on: Mar 22, 2024
+ *      Author: DELL
+ */
+
+
+#include "global.h"
+
+int TIME_NORMAL_RED = 5;
+int TIME_NORMAL_GREEN = 3;
+int TIME_NORMAL_YELLOW = 2;
+
+int statusLed = INIT;
+
+int count1 = 5;
+int count2 = 3;
+
+int countMan1 = 5;
+int countMan2 = 3;
+
+int mode = 1;
